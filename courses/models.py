438e5656e633184466a105715053cc2a257abd86
mohosin16103308 <hamza1610330816@gmail.com>
2019-09-06 10:09:36 +0600

    Create Courses Model and Migrated

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+#Courses Table Create
+
+class Courses(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
