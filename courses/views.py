ed82c4e4597337ba92eb2940c09107e801c4c3d6
mohosin16103308 <hamza1610330816@gmail.com>
2019-09-06 20:03:05 +0600

    Courses List View From Database

--- courses/views.py
+++ courses/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from django.http import Http404
+from django.shortcuts import render
+from .models import Courses
 # Create your views here.
 
 def index(request):
-    courses = ("PHP ","JAVA "," PYTHON")
-    return HttpResponse(courses)
+    courses = Courses.objects.all()
+    return render(request, 'courses/course_list.html', {'courses': courses}) 
