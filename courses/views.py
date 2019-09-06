d2d841bb379a49813c28f7d815d27c826adcd77d
mohosin16103308 <hamza1610330816@gmail.com>
2019-09-06 11:18:16 +0600

    Display Courses list from http Response

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    courses = ("PHP ","JAVA "," PYTHON")
+    return HttpResponse(courses)
