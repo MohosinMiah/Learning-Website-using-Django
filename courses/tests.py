25e553231f9bed60443e13c4fb2dfd72e06e2d0a
mohosin16103308 <hamza1610330816@gmail.com>
2019-09-08 08:58:40 +0600

    Test Course Model 01

--- courses/tests.py
+++ courses/tests.py
@@ -1,3 +1,23 @@
 from django.test import TestCase
 
+from .models import Courses
+
+from django.utils import timezone
+
+
 # Create your tests here.
+
+
+class CoursesModelTests(TestCase):
+    def test_course_creation(self):
+        course = Courses.objects.create(
+            title ="How to Test Model in django",
+            description ="Discussed details all in one "
+        )
+
+        now = timezone.now()
+
+        self.assertLessEqual(course.created_at,now)
+
+
+
