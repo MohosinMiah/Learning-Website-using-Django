109a3f01fb9ff90df942feaa4c509ae5d51399bd
mohosin16103308 <hamza1610330816@gmail.com>
2019-09-16 07:12:27 +0600

    Suggestion Form Validation using validators but using method

--- courses/forms.py
+++ courses/forms.py
@@ -3,13 +3,17 @@ from django.core import validators
 
 
 
+def must_be_empty(value):
+    if value:
+        raise forms.ValidationError("Must Be Empty")
+
 
 class SuggesstionForm(forms.Form):
     
     name = forms.CharField()
     email = forms.EmailField()
     suggestion = forms.CharField(widget=forms.Textarea)
-    honeypot = forms.CharField(required=False,widget=forms.HiddenInput,label="Leave Empty",validators=[validators.MaxLengthValidator(0)])
+    honeypot = forms.CharField(required=False,widget=forms.HiddenInput,label="Leave Empty",validators=[must_be_empty)
 
 
     def clean_honeypot(self):
