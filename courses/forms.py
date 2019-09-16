326037737a869d6e7dfac2e5cf6910550d1127bb
mohosin16103308 <hamza1610330816@gmail.com>
2019-09-16 07:23:30 +0600

    Validation Whole Form using clean method

--- courses/forms.py
+++ courses/forms.py
@@ -12,13 +12,28 @@ class SuggesstionForm(forms.Form):
     
     name = forms.CharField()
     email = forms.EmailField()
+    verify_email = forms.EmailField()
     suggestion = forms.CharField(widget=forms.Textarea)
-    honeypot = forms.CharField(required=False,widget=forms.HiddenInput,label="Leave Empty",validators=[must_be_empty)
+    honeypot = forms.CharField(required=False,widget=forms.HiddenInput,label="Leave Empty",validators=[must_be_empty])
 
 
-    def clean_honeypot(self):
-        data = self.cleaned_data["honeypot"]
-        if len(data):
-            raise forms.ValidationError("Leave It Empty")
-        return data
+# Clean method clean entire data 
+    def clean(self):
+# Get All Data
+        cleaned_data = super().clean()
+        email = cleaned_data.get('email')
+        verify_email = cleaned_data.get('verify_email')
+
+        if email != verify_email:
+            raise forms.ValidationError("Pls Enter Same EMail")
+
+
+
+
+
+    # def clean_honeypot(self):
+    #     data = self.cleaned_data["honeypot"]
+    #     if len(data):
+    #         raise forms.ValidationError("Leave It Empty")
+    #     return data
        
